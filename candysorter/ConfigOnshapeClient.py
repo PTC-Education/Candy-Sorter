f744b7ae8f3efa703b3635b55d943075849284bb
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-08-22 21:18:41 -0400

    doc filepath

--- candysorter/ConfigOnshapeClient.py
+++ candysorter/ConfigOnshapeClient.py
@@ -1,7 +1,7 @@
 from onshape_client.client import Client
 import json
 base = 'https://cad.onshape.com' # change this if you're using a document in an enterprise (i.e. "https://ptc.onshape.com")
-exec(open('~/Documents/colabkeys.py').read())
+exec(open('/Documents/colabkeys.py').read())
 client = Client(configuration={"base_url": base,
                                "access_key": access,
                                "secret_key": secret})
