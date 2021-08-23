382bc8cc84bbe986b06d26c8b9dd373e79943ee4
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-08-22 21:44:30 -0400

    global client

--- candysorter/__init__.py
+++ candysorter/__init__.py
@@ -494,6 +494,7 @@ def mainTraining(duplicationNum):
 ##
 
 def ConfigOnshapeClient():
+    global client
     from onshape_client.client import Client
     import json
     base = 'https://cad.onshape.com' # change this if you're using a document in an enterprise (i.e. "https://ptc.onshape.com")
