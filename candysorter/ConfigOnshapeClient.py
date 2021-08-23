43a1dcc33210298c2636ce63e43ffd7977f35270
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-08-22 21:35:15 -0400

    Input keys

--- candysorter/ConfigOnshapeClient.py
+++ candysorter/ConfigOnshapeClient.py
@@ -1,7 +1,8 @@
 from onshape_client.client import Client
 import json
 base = 'https://cad.onshape.com' # change this if you're using a document in an enterprise (i.e. "https://ptc.onshape.com")
-exec(open('/Documents/colabkeys.py').read())
+access = input('What is your access key?')
+secret = input('What is your secret key?')
 client = Client(configuration={"base_url": base,
                                "access_key": access,
                                "secret_key": secret})
